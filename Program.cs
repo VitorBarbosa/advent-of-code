e6de801fb636c528bc573a906a5172e6cf6a0d29
Vitor M. Barbosa <vitormbarbosa@gmail.com>
2024-12-13 13:40:20 -0300

    Day 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var left = new List<int>();
+var right = new List<int>();
+long distance = 0;
+
+var lines = File.ReadLines("../../../input");
+foreach (var line in lines) {
+    var numbers = line.Split(" ");
+    left.Add(int.Parse(numbers[0]));
+    right.Add(int.Parse(numbers[3]));
+}
+left.Sort();
+right.Sort();
+for (int i = 0; i < left.Count; i++)
+{
+    distance += Math.Abs(left[i] - right[i]);
+}
+Console.WriteLine(distance);
