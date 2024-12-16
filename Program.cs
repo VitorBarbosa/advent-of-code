a191e0274dcad6b45aa79cd73749abcdefaac05a
Vitor M. Barbosa <vitormbarbosa@gmail.com>
2024-12-16 14:53:59 -0300

    Day 2 - part 1

--- Program.cs
+++ Program.cs
@@ -1,23 +1,34 @@
-﻿var left = new List<int>();
-var right = new Dictionary<int, int>();
-long similarity = 0;
+﻿static bool isSafeReport(int[] numbers)
+{
+    bool isIncreasing = numbers[0] < numbers[1];
+    for (int i = 0; i < numbers.Length - 1; i++)
+    {
+        if ((isIncreasing && numbers[i] >= numbers[i + 1])
+        || (!isIncreasing && numbers[i] <= numbers[i + 1]))
+        {
+            return false;
+        }
+        var difference = Math.Abs(numbers[i] - numbers[i + 1]);
+        switch (difference)
+        {
+            case var _ when difference is 0 or > 3:
+                return false;
+            case var _ when difference is >= 1 and <= 3:
 
-var lines = File.ReadLines("../../../input");
+                break;
+        }
+    }
+    return true;
+}
+
+long safeReports = 0;
+
+var lines = File.ReadLines("../../../day2/input_day2");
 foreach (var line in lines)
 {
-    var numbers = line.Split(" ");
-    left.Add(int.Parse(numbers[0]));
-    if (!right.TryAdd(int.Parse(numbers[3]), 1))
-    {
-        right[int.Parse(numbers[3])]++;
-    }
+    var numbers = line.Split(" ").Select(int.Parse).ToArray();
+    if (isSafeReport(numbers))
+        safeReports++;
 }
-for (int i = 0; i < left.Count; i++)
-{
-    var currentNumber = left[i];
-    if (right.TryGetValue(currentNumber, out int count))
-    {
-        similarity += currentNumber * count;
-    }
-}
-Console.WriteLine(similarity);
+
+Console.WriteLine($"Result: {safeReports}");
